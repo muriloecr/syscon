c88595e6abde5fa50c8d30466f393e7b920fce6e
arley2023 <128830032+arley2023@users.noreply.github.com>
2024-09-08 18:15:57 -0300

    Update Concessionaria.sql
    sql

--- Syscon/Concessionaria.sql
+++ Syscon/Concessionaria.sql
@@ -38,6 +38,8 @@ create table Veiculo (
     vei_aces int(4),
 # ARMAZENA OS CLIENTES INTERESSADOS
 	vei_cli int(4),
+# ARMAZENA OS CONSULTORES
+	vei_con int(4),
 	vei_descricao varchar(250) not null,
     vei_gal longblob
 #   foreign key (vei_vis) references Cliente (cli_id)
@@ -52,15 +54,27 @@ create table Acessorios (
 #   foreign key (ace_vei) references Veiculo (vei_id)
 );
 insert into Consultor (con_id,con_nome,con_cli,con_vei) values 
-	(0,'0',0,0);
+	(0,'0',0,0),(1111,'jose durval',0001,0001),(2222,'maria do socorro',0002,0002),
+    (3333,'paulo sanches',0003,0001),(4444,'clara antunes',0006,0003);
 insert into Cliente (cli_id,cli_nome,cli_whats,cli_con,cli_vei) values 
-	(0,'0','0',0,0);
+	(0,'0','0',0,0),(0001,'carla maria','(85) 9.8947-8473',1111,0001),(0002,'jose abreu lima','(85) 9.4547-8473',4444,0004),
+	(0006,'pedro felipe','(85) 9.8057-8783',2222,0002),(0005,'rogerio mateus','(85) 9.844357-8783',3333,0002),
+    (0003,'edumondo dants','(85) 9.8457-8783',2222,0002),(0004,'marcio garci','(85) 9.8347-8876',3333,0003);
 insert into Acessorios (ace_id,ace_nome,ace_vei) values 
-	(0,'0',0);
+	(0,'0',0),(0001,'alarme',1001),(0002,'airbags',1001),
+    (0004,'som',1001),(0003,'gps',1001);
 insert into Veiculo (vei_id,vei_nome,vei_ano,vei_mod,vei_marca,
 	vei_condicao,vei_placa,vei_km,vei_combustivel,vei_cambio,
-	vei_carroc,vei_cor,vei_acess,vei_cli,vei_descricao,vei_gal) values 
-	(0,'0',0,0,'0','0','0',0,'0','0','0','0',0,0,'0','0');
+	vei_carroc,vei_cor,vei_acess,vei_cli,vei_con,vei_descricao,vei_gal) values 
+	(0,'0',0,0,'0','0','0',0,'0','0','0','0',0,0,0,'0',''),
+    (0001,'Saveiro',2020,2021,'volkswagen','usado','pns-9374',120.000,
+    'flex','maunual','pick-up','azul',0001,0001,1111,'pickup nova e revisada',''),
+    (0002,'Uno',2019,2020,'fiat','usado','kso-2586',180.000,
+    'flex','maunual','passeio','verde',0003,0006,4444,'muito economico',''),
+    (0003,'corsa',2019,2020,'gm','usado','ddo-2786',90.000,
+    'flex','automatico','uber','branco',0002,0002,2222,'top',''),
+    (0004,'ford k',2019,2020,'ford','novo','kso-2544',127.000,
+    'flex','automatico','anda','bege',0004,0004,3333,'ainda anda','');
 alter table Consultor add
 	foreign key (con_cli) references Cliente (cli_id);
 alter table Consultor add
