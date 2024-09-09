29193b3582cd4ab39f85ab6f2777bb9a7f9ae738
arley2023 <128830032+arley2023@users.noreply.github.com>
2024-09-09 20:15:28 -0300

    Update Concessionaria.sql
    up,

--- Syscon/Concessionaria.sql
+++ Syscon/Concessionaria.sql
@@ -2,14 +2,12 @@ create database Concessionaria;
 use Concessionaria;
 create table Consultor (
 	con_id int(4) unique not null primary key,
-    con_nome varchar(30) not null,
-	primary key(con_id)
+    con_nome varchar(30) not null
 );
 create table Cliente (
 	cli_id int(4) unique not null primary key,
     cli_nome varchar(30) not null,
-    cli_whats varchar(16) not null,
-    primary key(cli_id)
+    cli_whats varchar(16) not null
 );
 create table Veiculo (
 	vei_id int(4) unique not null primary key,
@@ -24,44 +22,37 @@ create table Veiculo (
     vei_cambio varchar(15) not null,
     vei_carroc varchar(15) not null,
     vei_cor varchar(15) not null,
-	vei_descricao varchar(250) not null,
+	vei_descricao varchar(250) not null
 #   vei_gal longblob
-	primary key(vei_id)
 );
 create table Acessorios (
 	ace_id int(4) unique not null primary key,
-    ace_nome varchar(30) not null,
-    primary key(ace_id)
+    ace_nome varchar(30) not null
 );
 create table ligacao (
-	ligacao_id int(12) not null,
+	ligacao_id bigint(12) not null primary key
 # ligacao_id = con_id + cli_id + vei_id
-	primary key(ligacao_id)
 );
 create table veiace (
-	veiace_id int(8) not null,
+	veiace_id int(8) not null primary key
 # veiace_id = vei_id + ace_id
-	primary key(veiace_id)
 );
 insert into Consultor (con_id,con_nome) values 
-	(0,'0'),
-    (1111,'jose durval'),
+	(1111,'jose durval'),
     (2222,'maria do socorro'),
     (3333,'paulo sanches'),
     (4444,'clara antunes');
 insert into Cliente (cli_id,cli_nome,cli_whats) values 
-	(0,'0','0'),
-    (9901,'carla maria','(85) 9.8947-8473'),
+	(9901,'carla maria','(85) 9.8947-8473'),
     (9902,'jose abreu lima','(85) 9.4547-8473'),
 	(9906,'pedro felipe','(85) 9.8057-8783'),
-    (9905,'rogerio mateus','(85) 9.844357-8783'),
+    (9905,'rogerio mateus','(85) 9.8447-8783'),
     (9903,'edumondo dants','(85) 9.8457-8783'),
     (9904,'marcio garci','(85) 9.8347-8876');
 insert into Veiculo (vei_id,vei_nome,vei_ano,vei_mod,vei_marca,
 	vei_condicao,vei_placa,vei_km,vei_combustivel,vei_cambio,
 	vei_carroc,vei_cor,vei_descricao) values 
-	(0,'0',0,0,'0','0','0',0,'0','0','0','0','0'),
-    (8801,'Saveiro',2020,2021,'volkswagen',
+	(8801,'Saveiro',2020,2021,'volkswagen',
     'usado','pns-9374',120.000,'flex','maunual',
     'pick-up','azul','pickup nova e revisada'),
     (8802,'Uno',2019,2020,'fiat',
@@ -73,14 +64,13 @@ insert into Veiculo (vei_id,vei_nome,vei_ano,vei_mod,vei_marca,
     (8804,'ford k',2019,2020,'ford',
     'zero','kso-2544',127.000,'flex','automatico',
     'lazer','bege','ainda anda');
-insert into Acessorios (ace_id,ace_nome,ace_vei) values 
-	(0,'0'),
-    (1001,'alarme'),
+insert into Acessorios (ace_id,ace_nome) values 
+	(1001,'alarme'),
     (2002,'airbags'),
     (3003,'som'),
     (4004,'gps');
-insert into ligacao (ligacao_id) values (0);
-insert into veiace (veiace_id) values (0);
+insert into ligacao (ligacao_id) values (111122223333);
+insert into veiace (veiace_id) values (10012002);
 select * from Consultor;
 select * from Cliente;
 select * from Veiculo;
