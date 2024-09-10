d1b0611ae6f493c5b9db404d14333c300fc83a01
arley2023 <128830032+arley2023@users.noreply.github.com>
2024-09-10 10:34:06 -0300

    Update rascunha.java
    aaa

--- Syscon/src/Concessionária/rascunha.java
+++ Syscon/src/Concessionária/rascunha.java
@@ -1,1 +1,16 @@
 
+// Código do cliente
+		JLabel labcodcli = new JLabel("Cliente cod.");
+		labcodcli.setFont(new Font("Tahoma", Font.BOLD, 24));
+		labcodcli.setBounds(45, 114, 150, 35);
+		frame.getContentPane().add(labcodcli);
+		
+		JLabel labcodcliform = new JLabel("formato 9999");
+		labcodcliform.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labcodcliform.setBounds(45, 139, 150, 28);
+		frame.getContentPane().add(labcodcliform);
+
+		clicod = new JFormattedTextField();
+		clicod.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		clicod.setBounds(229, 125, 160, 35);
+		frame.getContentPane().add(clicod);
