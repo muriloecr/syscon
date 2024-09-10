98b84b0c54a1370f9127501a886b925e96dbf906
arley2023 <128830032+arley2023@users.noreply.github.com>
2024-09-10 08:52:28 -0300

    Update conini.java
    upnew

--- Syscon/src/Concessionária/conini.java
+++ Syscon/src/Concessionária/conini.java
@@ -3,13 +3,44 @@ package Concessionaria;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
 
 public class conini {
+	
+// conecta BCO DADOS conecta BCO DADOS conecta BCO DADOS conecta BCO DADOS
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet resultset = null;
+	public void conectar() {
+		String servidor="jdbc:mysql://localhost:3306/cadastro";
+		String usuario = "";
+		String senha = "";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		try {
+			Class.forName(driver);
+			this.connection = DriverManager.getConnection(servidor,usuario,senha);
+			this.statement = this.connection.createStatement();
+		}catch(Exception e) {
+			System.out.println("ERROR: "+e.getMessage());
+		}
+	}
+	public boolean estaConectado() {
+		if(this.connection!=null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
+	
+	
 	private JFrame frame;
 
 // lança a APLICAÇÃO lança a APLICAÇÃO lança a APLICAÇÃO lança a APLICAÇÃO	
